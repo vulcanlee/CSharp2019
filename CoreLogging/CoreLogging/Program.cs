fe2a605fda264c2c864ab904494c3dc0a1ad4c2f
李進興 Vulcan <vulcan@miniasp.com>
2019-05-31 18:04:52 +0800

    .NET Core 主控台應用程式的 ILogger 寫入日誌使用練習

---
+++ CoreLogging/CoreLogging/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace CoreLogging
+{
+    class MyClass
+    {
+        private readonly ILogger<MyClass> logger;
+
+        public MyClass(ILogger<MyClass> logger)
+        {
+            this.logger = logger;
+        }
+        public void Method()
+        {
+            logger.LogInformation("現在正在執行 Method 方法");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var myClass = serviceProvider.GetService<MyClass>();
+
+            myClass.Method();
+
+            var logger = serviceProvider.GetService<ILogger<Program>>();
+            logger.LogError("Program 類別內發生了意外異常");
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging(configure => configure.AddConsole())
+                      .AddTransient<MyClass>();
+        }
+    }
+}
