4acbe95f9ec510770d19b802385975458ee1e633
李進興 Vulcan <vulcan@miniasp.com>
2019-06-12 00:21:51 +0800

    ASP.NET Core 在相依性注入容器，使用 具範圍與單一 註冊不同介面對應到同一個類別

---
+++ SameConcreteClass/SameConcreteClass/Controllers/ValuesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SameConcreteClass.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IMessageScope1 messageScope1;
+        private readonly IMessageScope2 messageScope2;
+        private readonly IMessageSingleton1 messageSingleton1;
+        private readonly IMessageSingleton2 messageSingleton2;
+
+        public ValuesController(IMessageScope1 messageScope1, IMessageScope2 messageScope2,
+            IMessageSingleton1 messageSingleton1, IMessageSingleton2 messageSingleton2)
+        {
+            this.messageScope1 = messageScope1;
+            this.messageScope2 = messageScope2;
+            this.messageSingleton1 = messageSingleton1;
+            this.messageSingleton2 = messageSingleton2;
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2",
+                messageScope1.Write("messageScope1"), messageScope2.Write("messageScope2"),
+                messageSingleton1.Write("messageSingleton1"), messageSingleton2.Write("messageSingleton2")
+            };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SameConcreteClass/SameConcreteClass/Startup.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace SameConcreteClass
+{
+    public interface IMessage
+    {
+        string Write(string message);
+    }
+    public interface IMessageScope1 : IMessage
+    {
+    }
+    public interface IMessageScope2 : IMessage
+    {
+    }
+    public interface IMessageSingleton1 : IMessage
+    {
+    }
+    public interface IMessageSingleton2 : IMessage
+    {
+    }
+    public class MessageClass : IMessageScope1, IMessageScope2, IMessageSingleton1, IMessageSingleton2
+    {
+        int HashCode;
+        public MessageClass()
+        {
+            HashCode = this.GetHashCode();
+            Console.WriteLine($"ConsoleMessage ({HashCode}) 已經被建立了");
+        }
+        public string Write(string message)
+        {
+            string result = $"[Console 輸出  ({HashCode})] {message}";
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IMessageScope1, MessageClass>();
+            services.AddScoped<IMessageScope2, MessageClass>();
+            services.AddSingleton<IMessageSingleton1, MessageClass>();
+            services.AddSingleton<IMessageSingleton2, MessageClass>();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
