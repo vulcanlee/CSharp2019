8af0183c9e6a262cb8fa3b9684b76426efdff01b
李進興 Vulcan <vulcan@miniasp.com>
2019-06-12 10:09:41 +0800

    ASP.NET Core 在相依性注入容器，使用 具範圍與單一 註冊不同介面對應到同一個類別

--- SameConcreteClass/SameConcreteClass/Controllers/ValuesController.cs
+++ SameConcreteClass/SameConcreteClass/Controllers/ValuesController.cs
@@ -33,6 +33,41 @@ namespace SameConcreteClass.Controllers
             };
         }
 
+        //private readonly IMessageScope1 messageScope1;
+        //private readonly IMessageScope2 messageScope2;
+        //private readonly IMessageSingleton1 messageSingleton1;
+        //private readonly IMessageSingleton2 messageSingleton2;
+        //private readonly IMessageScope1 messageScope01;
+        //private readonly IMessageScope2 messageScope02;
+        //private readonly IMessageSingleton1 messageSingleton01;
+        //private readonly IMessageSingleton2 messageSingleton02;
+
+        //public ValuesController(IMessageScope1 messageScope1, IMessageScope2 messageScope2,
+        //    IMessageSingleton1 messageSingleton1, IMessageSingleton2 messageSingleton2,
+        //    IMessageScope1 messageScope01, IMessageScope2 messageScope02,
+        //    IMessageSingleton1 messageSingleton01, IMessageSingleton2 messageSingleton02)
+        //{
+        //    this.messageScope1 = messageScope1;
+        //    this.messageScope2 = messageScope2;
+        //    this.messageSingleton1 = messageSingleton1;
+        //    this.messageSingleton2 = messageSingleton2;
+        //    this.messageScope01 = messageScope01;
+        //    this.messageScope02 = messageScope02;
+        //    this.messageSingleton01 = messageSingleton01;
+        //    this.messageSingleton02 = messageSingleton02;
+        //}
+        //// GET api/values
+        //[HttpGet]
+        //public ActionResult<IEnumerable<string>> Get()
+        //{
+        //    return new string[] { "value1", "value2",
+        //        messageScope1.Write("messageScope1"), messageScope2.Write("messageScope2"),
+        //        messageSingleton1.Write("messageSingleton1"), messageSingleton2.Write("messageSingleton2"),
+        //        messageScope01.Write("messageScope01"), messageScope02.Write("messageScope02"),
+        //        messageSingleton01.Write("messageSingleton01"), messageSingleton02.Write("messageSingleton02")
+        //    };
+        //}
+
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
