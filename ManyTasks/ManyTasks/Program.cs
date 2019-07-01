2ef9cf0fa831a1a606fba6a189d2b166cc1dd910
李進興 Vulcan <vulcan@miniasp.com>
2019-07-01 11:06:41 +0800

    C# 非同步作業中執行緒集區使用到大量的執行緒的設計考量

--- ManyTasks/ManyTasks/Program.cs
+++ ManyTasks/ManyTasks/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ManyTasks
@@ -9,40 +11,185 @@ namespace ManyTasks
     class Program
     {
         public static int MaxThreads;
+        static int MaxLoop = 200;
+        static int SimulateTaskTime = 2000;
+        static CountdownEvent  countdownEvent = new CountdownEvent(MaxLoop);
         static void Main(string[] args)
         {
-            //ThreadPool.SetMinThreads(50, 50);
-            int CurrentAvailableThreads;
-            CurrentAvailableThreads = GetAvailableThreads();
+            //Case1();
+            //Case2();
+            //Case3();
+            //Case4();
+            Case5();
+        }
+
+        private static void Case1()
+        {
+            int CurrentAvailableThreads = GetAvailableThreads();
             List<Task> allTask = new List<Task>();
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < MaxLoop; i++)
+            {
+                ThreadPool.QueueUserWorkItem(x =>
+                {
+                    int tmpThreadCC = CurrentAvailableThreads - GetAvailableThreads();
+                    if (MaxThreads < tmpThreadCC) MaxThreads = tmpThreadCC;
+                    Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}");
+                    Thread.Sleep(SimulateTaskTime);
+                    tmpThreadCC = CurrentAvailableThreads - GetAvailableThreads();
+                    if (MaxThreads < tmpThreadCC) MaxThreads = tmpThreadCC;
+                    countdownEvent.Signal();
+                });
+            }
+
+            countdownEvent.Wait();
+            stopwatch.Stop();
+
+            PrintThreadPoolInformation();
+            Console.WriteLine($"此次使用到 {MaxThreads} 個背景執行緒");
+            Console.WriteLine($"此次共花費 {stopwatch.Elapsed} 時間");
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+
+        private static void Case2()
+        {
+            int CurrentAvailableThreads = GetAvailableThreads();
+            List<Task> allTask = new List<Task>();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < MaxLoop; i++)
             {
                 allTask.Add(Task.Run(() =>
                 {
                     int tmpThreadCC = CurrentAvailableThreads - GetAvailableThreads();
                     if (MaxThreads < tmpThreadCC) MaxThreads = tmpThreadCC;
                     Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}");
-                    Thread.Sleep(2000);
+                    Thread.Sleep(SimulateTaskTime);
                     tmpThreadCC = CurrentAvailableThreads - GetAvailableThreads();
                     if (MaxThreads < tmpThreadCC) MaxThreads = tmpThreadCC;
+                    countdownEvent.Signal();
                 }));
             }
 
-            Task.WhenAll(allTask).Wait();
+            countdownEvent.Wait();
             stopwatch.Stop();
+
+            PrintThreadPoolInformation();
             Console.WriteLine($"此次使用到 {MaxThreads} 個背景執行緒");
             Console.WriteLine($"此次共花費 {stopwatch.Elapsed} 時間");
             Console.WriteLine("Press any key for continuing...");
             Console.ReadKey();
         }
+
+        private static void Case3()
+        {
+            ThreadPool.SetMinThreads(16, 16);
+            int CurrentAvailableThreads = GetAvailableThreads();
+            List<Task> allTask = new List<Task>();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < MaxLoop; i++)
+            {
+                allTask.Add(Task.Run(() =>
+                {
+                    int tmpThreadCC = CurrentAvailableThreads - GetAvailableThreads();
+                    if (MaxThreads < tmpThreadCC) MaxThreads = tmpThreadCC;
+                    Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}");
+                    Thread.Sleep(SimulateTaskTime);
+                    tmpThreadCC = CurrentAvailableThreads - GetAvailableThreads();
+                    if (MaxThreads < tmpThreadCC) MaxThreads = tmpThreadCC;
+                    countdownEvent.Signal();
+                }));
+            }
+
+            countdownEvent.Wait();
+            stopwatch.Stop();
+
+            PrintThreadPoolInformation();
+            Console.WriteLine($"此次使用到 {MaxThreads} 個背景執行緒");
+            Console.WriteLine($"此次共花費 {stopwatch.Elapsed} 時間");
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+
+        private static void Case4()
+        {
+            int CurrentAvailableThreads = GetAvailableThreads();
+            List<Task> allTask = new List<Task>();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < MaxLoop; i++)
+            {
+                allTask.Add(Task.Factory.StartNew(() =>
+                {
+                    int tmpThreadCC = CurrentAvailableThreads - GetAvailableThreads();
+                    if (MaxThreads < tmpThreadCC) MaxThreads = tmpThreadCC;
+                    Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}");
+                    Thread.Sleep(SimulateTaskTime);
+                    tmpThreadCC = CurrentAvailableThreads - GetAvailableThreads();
+                    if (MaxThreads < tmpThreadCC) MaxThreads = tmpThreadCC;
+                    countdownEvent.Signal();
+                }, TaskCreationOptions.LongRunning));
+            }
+
+            countdownEvent.Wait();
+            stopwatch.Stop();
+
+            PrintThreadPoolInformation();
+            Console.WriteLine($"此次使用到 {MaxThreads} 個背景執行緒");
+            Console.WriteLine($"此次共花費 {stopwatch.Elapsed} 時間");
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+
+        private static void Case5()
+        {
+            int CurrentAvailableThreads = GetAvailableThreads();
+            List<Task> allTask = new List<Task>();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < MaxLoop; i++)
+            {
+                allTask.Add(Task.Run(async () =>
+                {
+                    int tmpThreadCC = CurrentAvailableThreads - GetAvailableThreads();
+                    if (MaxThreads < tmpThreadCC) MaxThreads = tmpThreadCC;
+                    Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}");
+                    await Task.Delay(SimulateTaskTime);
+                    tmpThreadCC = CurrentAvailableThreads - GetAvailableThreads();
+                    if (MaxThreads < tmpThreadCC) MaxThreads = tmpThreadCC;
+                    countdownEvent.Signal();
+                }));
+            }
+
+            countdownEvent.Wait();
+            stopwatch.Stop();
+
+            PrintThreadPoolInformation();
+            Console.WriteLine($"此次使用到 {MaxThreads} 個背景執行緒");
+            Console.WriteLine($"此次共花費 {stopwatch.Elapsed} 時間");
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+
         public static int GetAvailableThreads()
         {
             int workerThreads;
             int completionPortThreads;
             ThreadPool.GetAvailableThreads(out workerThreads, out completionPortThreads);
             return workerThreads;
+        }
+        public static void PrintThreadPoolInformation()
+        {
+            int workerThreads;
+            int completionPortThreads;
+            ThreadPool.GetMaxThreads(out workerThreads, out completionPortThreads);
+            Console.WriteLine($"執行緒集區中的背景工作執行緒最大數目 : {workerThreads} / 執行緒集區中的非同步 I/O 執行緒最大數目 : { completionPortThreads}");
+            ThreadPool.GetMinThreads(out workerThreads, out completionPortThreads);
+            Console.WriteLine($"需要建立的背景工作執行緒最小數目 : {workerThreads} / 需要建立的非同步 I/O 執行緒最小數目 : { completionPortThreads}");
+            Console.WriteLine($"");
         }
     }
 }
