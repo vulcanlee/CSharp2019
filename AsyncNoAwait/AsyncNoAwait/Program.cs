f1a85c53797c013dbecf12a4550a18f5362c8f79
李進興 Vulcan <vulcan@miniasp.com>
2019-07-30 12:22:09 +0800

    C# 對於有使用修飾詞 async 非同步方法內，有沒有使用 await 關鍵字，其這兩種方法會有何差異呢？

--- AsyncNoAwait/AsyncNoAwait/Program.cs
+++ AsyncNoAwait/AsyncNoAwait/Program.cs
@@ -12,9 +12,10 @@ namespace AsyncNoAwait
         static async Task Main(string[] args)
         {
             Console.WriteLine($"1 ({Thread.CurrentThread.ManagedThreadId})");
-            string result = await MyMethodAsync();
+            var task= MyMethodAsync();
             Console.WriteLine($"2 ({Thread.CurrentThread.ManagedThreadId})");
 
+            string result = await task;
             Console.WriteLine($"呼叫非同步方法結果 {result}");
 
             Console.WriteLine("Press any key for continuing...");
@@ -25,8 +26,10 @@ namespace AsyncNoAwait
             Console.WriteLine($"進入到非同步方法");
             Console.WriteLine($"3 ({Thread.CurrentThread.ManagedThreadId})");
 
-            //Thread.Sleep(3000);
-            await Task.Delay(3000);
+            // 測試條件1: 在 async 方法內，沒有使用到任何的 await 運算子
+            Thread.Sleep(3000);
+            // 測試條件2: 在 async 方法內，使用到 await 運算子
+            //await Task.Delay(3000);
 
             Console.WriteLine($"準備離開到非同步方法");
             Console.WriteLine($"4 ({Thread.CurrentThread.ManagedThreadId})");
