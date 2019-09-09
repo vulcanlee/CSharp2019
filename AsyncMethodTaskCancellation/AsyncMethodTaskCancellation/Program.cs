5ce8853f5e12ad7ca7b9cd6c88173ac45341fcaf
李進興 Vulcan <vulcan@miniasp.com>
2019-09-09 12:17:39 +0800

    C# 當要呼叫多個連續非同步作業的各種不同設計方式

---
+++ AsyncMethodTaskCancellation/AsyncMethodTaskCancellation/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncMethodTaskCancellation
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken token = cts.Token;
+
+            Task fooAsyncTask = Task.Run(() =>
+            {
+                MyMethod(token);
+            }, token);
+
+            // 這裡在呼叫 Task.Run 方法的時候，第二個參數沒有傳遞 CancellationToken 物件
+            Task fooAsyncTaskWithoutToken = Task.Run(() =>
+            {
+                MyMethod(token);
+            });
+
+            Task fooAsyncMethod = MyMethodAsync(token);
+
+            await Task.Delay(500);
+
+            cts.Cancel();
+
+            await Task.Delay(1000);
+
+            Console.WriteLine($"非同步工作的狀態: IsCompleted={fooAsyncTask.IsCompleted} , " +
+                $"IsCanceled={fooAsyncTask.IsCanceled} , IsFaulted={fooAsyncTask.IsFaulted}");
+            Console.WriteLine($"非同步工作(沒有傳送取消權杖)的狀態: IsCompleted={fooAsyncTaskWithoutToken.IsCompleted} , " +
+                $"IsCanceled={fooAsyncTaskWithoutToken.IsCanceled} , IsFaulted={fooAsyncTaskWithoutToken.IsFaulted}");
+            Console.WriteLine($"非同步方法的狀態: IsCompleted={fooAsyncMethod.IsCompleted} , " +
+                $"IsCanceled={fooAsyncMethod.IsCanceled} , IsFaulted={fooAsyncMethod.IsFaulted}");
+
+        }
+        static async Task MyMethodAsync(CancellationToken token)
+        {
+            await Task.Delay(1000);
+            token.ThrowIfCancellationRequested();
+        }
+        static void MyMethod(CancellationToken token)
+        {
+            Thread.Sleep(1000);
+            token.ThrowIfCancellationRequested();
+        }
+    }
+}
---
+++ MultiAsyncCombine/MultiAsyncCombine/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiAsyncCombine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("這裡將會展示當要呼叫多個非同步作業的各種不同設計方式");
+
+            // 這裡寫法的特色：使用一行敘述 (Task 支援 fluent API 寫法)，就可以完成所有設計
+            //使用工作ContinueWith();
+
+            // 這裡寫法的特色：需要使用封鎖式等待非同步作業(可能是非同步工作或者非同步方法)的完成
+            同步封鎖等待非同步作業完成();
+
+            // 這裡寫法的特色：使用 async 修飾詞 與 await 運算子 ，採用不會直接封鎖的方式來呼叫非同步作業
+            // 重點是可以使用同步設計的風格和方式，設計出具有非同步運作能力的程式碼
+            //await呼叫非同步方法Async().Wait();
+        }
+
+        private static void 同步封鎖等待非同步作業完成()
+        {
+            string url = "https://lobworkshop.azurewebsites.net/api/RemoteSource/AddASync/15/43/3000";
+            string content = new HttpClient().GetStringAsync(url).Result;
+            var task2 = File.WriteAllTextAsync("同步封鎖等待完成.txt", content);
+            task2.Wait();
+            if (task2.Status == TaskStatus.RanToCompletion)
+            {
+                Console.WriteLine("已經成功下載內容並且寫入到檔案內");
+            }
+            else
+            {
+                Console.WriteLine("寫入檔案發生了問題");
+            }
+        }
+
+        private static void 使用工作ContinueWith()
+        {
+            string url = "https://lobworkshop.azurewebsites.net/api/RemoteSource/AddASync/15/43/3000";
+            var myTask = new HttpClient().
+                GetStringAsync(url).ContinueWith(task1 =>
+                {
+                    string content = task1.Result;
+                    File.WriteAllTextAsync("MyFileContinueWith.txt", content).
+                    ContinueWith(task2 =>
+                    {
+                        if (task2.Status == TaskStatus.RanToCompletion)
+                        {
+                            Console.WriteLine("已經成功下載內容並且寫入到檔案內");
+                        }
+                        else
+                        {
+                            Console.WriteLine("寫入檔案發生了問題");
+                        }
+                    });
+                });
+
+            Thread.Sleep(4000);
+        }
+
+        private static async Task await呼叫非同步方法Async()
+        {
+            string content = await GetRemoteStringAsync();
+            await WriteRemoteStringAsync("MyFile.txt", content);
+        }
+
+        static async Task<string> GetRemoteStringAsync()
+        {
+            string url = "https://lobworkshop.azurewebsites.net/api/RemoteSource/AddASync/15/43/3000";
+            string result = await new HttpClient().GetStringAsync(url);
+            return result;
+        }
+        static async Task WriteRemoteStringAsync(string filename, string content)
+        {
+            await File.WriteAllTextAsync(filename, content);
+        }
+    }
+}
