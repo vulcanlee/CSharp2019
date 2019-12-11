c66051c493a81b233141d1da2a36fe67060e5b6f
李進興 Vulcan <vulcan@miniasp.com>
2019-12-11 19:56:28 +0800

    從空白專案來建立 ASP.NET Core Blazor 應用程式 - 了解 Blazor 運作方式

--- BlazorFromeEmpty/BlazorFromeEmpty/Startup.cs
+++ BlazorFromeEmpty/BlazorFromeEmpty/Startup.cs
@@ -17,9 +17,12 @@ namespace BlazorFromeEmpty
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            #region 在這裡要宣告所要使用的服務
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            // 設著將這行註解起來，並且執行，看看會發生甚麼問題？
             services.Configure<RazorPagesOptions>(options => options.RootDirectory = "/");
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,8 +33,10 @@ namespace BlazorFromeEmpty
                 app.UseDeveloperExceptionPage();
             }
 
+            #region 請加入這兩行敘述
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            #endregion
 
             app.UseRouting();
 
