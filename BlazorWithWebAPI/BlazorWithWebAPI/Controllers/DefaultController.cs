fb2cc90fb82f1a4a87bd8445108cc85d8bca91de
李進興 Vulcan <vulcan@miniasp.com>
2019-12-13 17:59:43 +0800

    在 ASP.NET Core Blazor 專案內，加上 Web API 服務

---
+++ BlazorWithWebAPI/BlazorWithWebAPI/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWithWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello~~";
+        }
+    }
+}
